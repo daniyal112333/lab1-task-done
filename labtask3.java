495f7ec344fec09a2fbb48afcc847964d734f06a
daniyal112333 <117898642+daniyal112333@users.noreply.github.com>
2022-11-10 13:27:55 +0500

    Add files via upload

---
+++ labtask3.java
@@ -0,0 +1,15 @@
+class labtask3
+
+{
+  public static void main(String[] args) {
+    
+
+    int a = 12, b = 5;
+
+    System.out.println("a + b = " + (a + b));
+    System.out.println("a - b = " + (a - b));
+    System.out.println("a * b = " + (a * b));
+    System.out.println("a / b = " + (a / b));
+    System.out.println("a % b = " + (a % b));
+  }
+}
